607c07d89de74658f0534a7cab26b735bb10219c
tiagopereira <tiagopereira@me.com>
2026-03-12 14:20:18 -0300

    Add neve.lua with bug fixes

---
+++ neve.lua
@@ -0,0 +1,6 @@
+-- NEVE
+-- vocal chain: transient → preamp → comp → soothe → air
+-- enc1: intensity  |  k1: clip mode  |  k2: mono  |  k3: bypass
+-- inspired by Rupert Neve 1073, SSL comp, Soothe2
+
+engine.name = "Neve"
